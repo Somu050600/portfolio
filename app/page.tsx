071e89382e2e698c1df166bdd4b75fa9ac6838af
Eega Somasekhara Reddy <somasekhareega@gmail.com>
2024-03-14 11:20:44 +0530

    minor scroling bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-4 overflow-x-hidden">
+    <main className="flex min-h-screen flex-col items-center justify-between px-4 overflow-hidden">
       <Hero />
       <Projects />
       <Work />
