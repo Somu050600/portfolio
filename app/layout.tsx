4362ad0d32d5e9c2de9107686e815a39839845bb
Eega Somasekhara Reddy <somasekhareega@gmail.com>
2024-03-18 22:50:56 +0530

    minor code change

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,10 +59,10 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Navabr />
+          <SpeedInsights />
           <main>{children}</main>
           <Toaster />
         </ThemeProvider>
-        <SpeedInsights />
       </body>
     </html>
   );
