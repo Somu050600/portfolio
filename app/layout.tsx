5660c26904ac816e81a146d5dad6fc552c95bec7
Eega Somasekhara Reddy <somasekhareega@gmail.com>
2024-03-22 23:03:25 +0530

    hiden the scrollbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className + " scrollbar-hide"}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
