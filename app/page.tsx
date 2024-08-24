a5ffa4d2ceb416fb7eb75afa71308f8ed9e6b537
Eega Somasekhara Reddy <119160374+Somu050600@users.noreply.github.com>
2024-08-24 23:47:14 +0530

    added cloudsek exp

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-4 overflow-hidden">
       <Hero />
-      <Projects />
       <Work />
+      <Projects />
       <Skills />
       <Contact />
       <span
